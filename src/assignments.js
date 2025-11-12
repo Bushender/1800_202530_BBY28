30aa0ef111511366718fec69ef9e3eac43ae7837
SadRectangle <graceyang803@gmail.com>
2025-11-11 22:00:26 -0800

    the edit and delete button of the meatball menu for the assignments is now functioning

--- src/assignments.js
+++ src/assignments.js
@@ -6,7 +6,8 @@ import {
   getDoc,
   getDocs,
   addDoc,
-  setDoc,
+  updateDoc,
+  deleteDoc,
   query,
   where,
 } from "firebase/firestore";
@@ -27,16 +28,28 @@ const create = document.getElementById("createBtn");
 const cancel = document.getElementById("cancelBtn");
 const overlay = document.getElementById("overlay");
 
+const edit = document.getElementById("editBtn");
+const editForm = document.getElementById("editForm");
+const cancelEdit = document.getElementById("cancelEditBtn");
+const container = document.getElementById("assignmentContainer");
+
+cancel.addEventListener("click", closeForm);
+
+cancelEdit.addEventListener("click", closeForm);
+
 function closeForm() {
   assignmentForm.style.display = "none";
+  editForm.style.display = "none";
   overlay.style.display = "none";
 }
 
+// Opens the create form
 add.addEventListener("click", () => {
   assignmentForm.style.display = "flex";
   overlay.style.display = "block";
 });
 
+// The create assignment form and sending it to firebase
 create.addEventListener("click", async () => {
   const user = getAuth().currentUser;
 
@@ -59,6 +72,7 @@ create.addEventListener("click", async () => {
   loadAssignments(user);
 });
 
+// Loading all set assignments to assignments page
 async function loadAssignments(user) {
   const userRef = doc(db, "users", user.uid);
   const userSnap = await getDoc(userRef);
@@ -74,13 +88,21 @@ async function loadAssignments(user) {
   setAssignments.forEach((assignment) => {
     const data = assignment.data();
     const newAssignment = document.createElement("div");
+
     newAssignment.classList.add("assignmentItem");
+    newAssignment.dataset.id = assignment.id;
+
     newAssignment.innerHTML = `
     <div class="top">${data.className}
       <button class="menu" type="button">
         <img src="images/dots.png" alt="menu" class="dots">
       </button>
     </div>
+    <ul class="detailsDropdown dropdown-menu dropdown-menu-end">
+      <li><button class="editBtn">Edit</button></li>
+      <li class="dropdown-divider"> </li>
+      <li><button class="deleteBtn">Delete</button></li>
+    </ul>
     <div class="bottom">${data.name}
     <span>Due: ${data.dueDate}</span>
     </div>
@@ -89,8 +111,65 @@ async function loadAssignments(user) {
   });
 }
 
-cancel.addEventListener("click", closeForm);
+// menu control (edit, delete, save edit)
+container.addEventListener("click", async (e) => {
+  const target = e.target;
+  const assignment = target.closest(".assignmentItem");
+  if (!assignment) {
+    return;
+  }
+  const assignmentID = assignment.dataset.id;
+  const dropdown = assignment.querySelector(".detailsDropdown");
 
-// menubar.addEventListener("click", () => {
-//   //
-// });
+  if (target.closest(".menu")) {
+    dropdown.style.display =
+      dropdown.style.display === "none" ? "flex" : "none";
+  }
+
+  if (
+    !target.closest(".menu") &&
+    !target.closest(".editBtn") &&
+    !target.closest(".deleteBtn")
+  ) {
+    dropdown.style.display = "none";
+  }
+
+  if (target.closest(".deleteBtn")) {
+    await deleteDoc(doc(db, "assignments", assignmentID));
+    loadAssignments(getAuth().currentUser);
+    return;
+  }
+
+  if (target.closest(".editBtn")) {
+    const className = document.getElementById("classEdit");
+    const name = document.getElementById("assignmentEdit");
+    const dueDate = document.getElementById("dateEdit");
+
+    const docRef = doc(db, "assignments", assignmentID);
+    const docSnap = await getDoc(docRef);
+    const data = docSnap.data();
+
+    editForm.style.display = "flex";
+    overlay.style.display = "block";
+
+    className.value = data.className;
+    name.value = data.name;
+    dueDate.value = data.dueDate;
+
+    document.getElementById("saveBtn").onclick = async () => {
+      const classValue = className.value;
+      const assignmentValue = name.value;
+      const dateValue = dueDate.value;
+      const docRef = doc(db, "assignments", assignmentID);
+
+      await updateDoc(docRef, {
+        className: classValue,
+        name: assignmentValue,
+        dueDate: dateValue,
+      });
+
+      closeForm();
+      loadAssignments(getAuth().currentUser);
+    };
+  }
+});
