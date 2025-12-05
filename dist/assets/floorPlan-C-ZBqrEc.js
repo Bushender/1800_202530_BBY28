8b3a6bbb3edda8ef9eab596c46c0990eaeb7fe21
Thien Pham <tpham139@my.bcit.ca>
2025-12-04 23:47:10 -0800

    Delete dist directory

--- dist/assets/floorPlan-C-ZBqrEc.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener("click",t=>{if(!t.target.classList.contains("floor-btn"))return;const o=t.target.getAttribute("data-floor");o&&(window.location.href=`/floorPlaceholder.html?floor=${o}`)});
